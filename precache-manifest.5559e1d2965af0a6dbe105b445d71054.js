0cdfd5a3d04a991c0afc074bb0bd96b9e274177a
kakuttaja <40384624+kakuttaja@users.noreply.github.com>
2023-06-06 18:15:48 +0900

    Updates

---
+++ precache-manifest.5559e1d2965af0a6dbe105b445d71054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2829ce672beb76e73f7b",
+    "url": "/static/css/main.5eb91832.chunk.css"
+  },
+  {
+    "revision": "2829ce672beb76e73f7b",
+    "url": "/static/js/main.2829ce67.chunk.js"
+  },
+  {
+    "revision": "3d5d039f2ec4da49612f",
+    "url": "/static/js/1.3d5d039f.chunk.js"
+  },
+  {
+    "revision": "a707b05919d1a5fef3b4",
+    "url": "/static/js/2.a707b059.chunk.js"
+  },
+  {
+    "revision": "aed341847dc440cb1703",
+    "url": "/static/js/runtime~main.aed34184.js"
+  },
+  {
+    "revision": "38c593f4d64b028bad2a87a1350f823b",
+    "url": "/static/media/catto3.38c593f4.jpg"
+  },
+  {
+    "revision": "d9fac0f26f490ca7c0d3609e0b4164b4",
+    "url": "/index.html"
+  }
+];
